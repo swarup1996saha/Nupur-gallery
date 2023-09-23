9cd5c5b7ad5adf8c952d8b8cebbaa3be645ef5a7
swarup1996saha <hoichoi1018@gmail.com>
2023-09-23 15:49:46 +0530

    again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,43 +13,43 @@ export class AppComponent {
 
   data: Item[] = [
     {
-      imageSrc: '../assets/images/image1.jpg',
+      imageSrc: '../assets/images/1.jpg',
       imageAlt: '1'
     },
     {
-      imageSrc: '../assets/images/image3.jpg',
+      imageSrc: '../assets/images/2.jpg',
       imageAlt: '2'
     },
     {
-      imageSrc: '../assets/images/image2.jpg',
+      imageSrc: '../assets/images/3.jpg',
       imageAlt: '3'
     },
     {
-      imageSrc: '../assets/images/image4.jpg',
+      imageSrc: '../assets/images/4.jpg',
       imageAlt: '4'
     },
     {
-      imageSrc: '../assets/images/image5.jpg',
+      imageSrc: '../assets/images/5.jpg',
       imageAlt: '5'
     },
     {
-      imageSrc: '../assets/images/image6.jpg',
+      imageSrc: '../assets/images/6.jpg',
       imageAlt: '6'
     },
     {
-      imageSrc: '../assets/images/image7.jpg',
+      imageSrc: '../assets/images/7.jpg',
       imageAlt: '7'
     },
     {
-      imageSrc: '../assets/images/image8.jpg',
+      imageSrc: '../assets/images/8.jpg',
       imageAlt: '8'
     },
     {
-      imageSrc: '../assets/images/image9.jpg',
+      imageSrc: '../assets/images/9.jpg',
       imageAlt: '9'
     },
     {
-      imageSrc: '../assets/images/image10.jpg',
+      imageSrc: '../assets/images/10.jpg',
       imageAlt: '10'
     },
     {
@@ -57,71 +57,99 @@ export class AppComponent {
       imageAlt: '11'
     },
     {
-      imageSrc: '../assets/images/13.jpg',
+      imageSrc: '../assets/images/12.jpg',
       imageAlt: '12'
     },
     {
-      imageSrc: '../assets/images/15.jpg',
+      imageSrc: '../assets/images/13.jpg',
       imageAlt: '13'
     },
     {
-      imageSrc: '../assets/images/17.jpg',
+      imageSrc: '../assets/images/14.jpg',
       imageAlt: '14'
     },
     {
-      imageSrc: '../assets/images/19.jpg',
+      imageSrc: '../assets/images/15.jpg',
       imageAlt: '15'
     },
     {
-      imageSrc: '../assets/images/21.jpg',
+      imageSrc: '../assets/images/16.jpg',
       imageAlt: '16'
     },
     {
-      imageSrc: '../assets/images/23.jpg',
+      imageSrc: '../assets/images/17.jpg',
       imageAlt: '17'
     },
     {
-      imageSrc: '../assets/images/29.jpg',
+      imageSrc: '../assets/images/18.jpg',
       imageAlt: '18'
     },
     {
-      imageSrc: '../assets/images/31.jpg',
+      imageSrc: '../assets/images/19.jpg',
       imageAlt: '19'
     },{
-      imageSrc: '../assets/images/12.jpg',
+      imageSrc: '../assets/images/20.jpg',
       imageAlt: '20'
     },
     {
-      imageSrc: '../assets/images/14.jpg',
+      imageSrc: '../assets/images/21.jpg',
       imageAlt: '21'
     },
     {
-      imageSrc: '../assets/images/16.jpg',
+      imageSrc: '../assets/images/22.jpg',
       imageAlt: '22'
     },
     {
-      imageSrc: '../assets/images/30.jpg',
+      imageSrc: '../assets/images/23.jpg',
       imageAlt: '23'
     },
     {
-      imageSrc: '../assets/images/18.jpg',
+      imageSrc: '../assets/images/24.jpg',
       imageAlt: '24'
     },
     {
-      imageSrc: '../assets/images/20.jpg',
+      imageSrc: '../assets/images/25.jpg',
       imageAlt: '25'
     },
     {
-      imageSrc: '../assets/images/22.jpg',
+      imageSrc: '../assets/images/26.jpg',
       imageAlt: '26'
     },
     {
-      imageSrc: '../assets/images/26.jpg',
+      imageSrc: '../assets/images/27.jpg',
       imageAlt: '27'
     },
     {
       imageSrc: '../assets/images/28.jpg',
       imageAlt: '28'
+    },
+    {
+      imageSrc: '../assets/images/29.jpg',
+      imageAlt: '29'
+    },
+    {
+      imageSrc: '../assets/images/30.jpg',
+      imageAlt: '30'
+    },
+    {
+      imageSrc: '../assets/images/31.jpg',
+      imageAlt: '31'
+    },
+    {
+      imageSrc: '../assets/images/32.jpg',
+      imageAlt: '32'
+    },
+    {
+      imageSrc: '../assets/images/33.jpg',
+      imageAlt: '33'
+    },
+    {
+      imageSrc: '../assets/images/34.jpg',
+      imageAlt: '34'
+    },
+    {
+      imageSrc: '../assets/images/35.jpg',
+      imageAlt: '35'
     },
 
   ]
