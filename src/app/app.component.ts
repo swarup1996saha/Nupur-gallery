f82d78e02ac1f5595cedd9313e6661992d77d610
Swarup Saha <hoichoi1018@gmail.com>
2025-02-17 16:38:51 +0530

    selfie added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,8 +28,17 @@ export class AppComponent {
     }));
   }
 
+  private generateSelfieImages(count: number): Item[] {
+    return Array.from({ length: count }, (_, i) => ({
+      imageSrc: `../assets/images/Selfie/3 (${i + 1}).jpg`,
+      imageAlt: `Selfie ${i + 1}`,
+      category: "Selfies"
+    }));
+  }
+
   data: Item[] = [
     ...this.generateAnnieWeddingImages(67),  // Generate 67 wedding images
     ...this.generateAbhijeetWeddingImages(16),  // Generate 67 wedding images
+    ...this.generateSelfieImages(65),  // Adjust the number based on your actual selfie count
   ];
 }
